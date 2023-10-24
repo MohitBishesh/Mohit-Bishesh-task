45de219c3f8b28b2a729b984ce811787c1f0a9b3
Mohit Bishesh <74617917+MohitBishesh@users.noreply.github.com>
2023-10-24 16:58:48 +0530

    Create variables.tf
    creating variable for ec2-ami, and database

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "ec2-ubuntu-ami" {
+ default = "ami-053b0d53c279acc90"
+}
+
+variable "ec2-linux-ami" {
+ default = "ami-041feb57c611358bd"
+}
+
+variable "ec2-macos-ami" {
+ default = "ami-0051c80cce1c6bbcc"
+}
+variable "db_name" {
+  type    = string
+}
+
+variable "db_username" {
+  type    = string
+}
+
+variable "db_password" {
+  type    = string
+}
